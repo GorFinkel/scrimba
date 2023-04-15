1011ba3d0bd780a75836b414a1d5c5ee45e3f9ca
GorFinkel <97066710+GorFinkel@users.noreply.github.com>
2023-04-15 23:18:43 +0300

    Update panic.js

--- interviewChallenges/panic.js
+++ interviewChallenges/panic.js
@@ -1,15 +1,57 @@
-function panic(string){
-    array=string.split(' ')
-    if(array.length){
-        for (let str=0;str<array.length-1;str++){
-            array[str]=`${array[str]} 😱`
-        }
-    }
-    array.push('!')
-    string=array.join(' ')
-    return string
+
+
+
+/* Panic function 
+Write a PANIC! function. The function should take in a sentence and return the same
+sentence in all caps with an exclamation point (!) at the end. Use JavaScript's
+built in string methods. 
+
+If the string is a phrase or sentence, add a 😱 emoji in between each word. 
+
+Example input: "Hello"
+Example output: "HELLO!"
+
+Example input: "I'm almost out of coffee"
+Example output: "I'M 😱 ALMOST 😱 OUT 😱 OF 😱 COFFEE!"
+
+.split() .join()
+*/
+function panic(str){
+    return str
+        .split(' ')
+        .join(' 😱 ')
+        .toUpperCase() + "!";
 }
 
 // Test your function
 console.log(panic("I'm almost out of coffee")); 
 console.log(panic("winter is coming"))
+
+
+function panic(str){
+    return str
+        .split(' ')
+        .join(' 😱 ')
+        .toUpperCase() + "!";
+}
+
+// Test your function
+console.log(panic("I'm almost out of coffee")); 
+console.log(panic("winter is coming"))
+
+
+//function panic(string){
+//     array=string.split(' ')
+//     if(array.length){
+//         for (let str=0;str<array.length-1;str++){
+//             array[str]=`${array[str]} 😱`
+//         }
+//     }
+//     array.push('!')
+//     string=array.join(' ')
+//     return string
+// }
+
+// // Test your function
+// console.log(panic("I'm almost out of coffee")); 
+// console.log(panic("winter is coming"))
